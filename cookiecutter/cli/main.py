6fc3432b463817cb39da6af34f87ed367a3b54ba
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-12-26 21:42:26 +0800

    Add `ls` command
    Resolves #8

--- cookiecutter/cli/main.py
+++ cookiecutter/cli/main.py
@@ -5,6 +5,7 @@
 import click
 
 from new import new
+from ls import ls
 
 
 @click.group()
@@ -29,6 +30,7 @@ def main(ctx, verbose, debug_file):
 
 
 main.add_command(new)
+main.add_command(ls)
 
 
 if __name__ == "__main__":
