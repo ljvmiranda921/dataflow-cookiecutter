e2cc03165d3d25c2cd135dc0d7ebad22d9b8793a
Lester James V. Miranda <ljvmiranda@gmail.com>
2019-12-26 23:57:03 +0800

    Use python file instead of JSON for config

--- dataflow_cookiecutter/cli/ls.py
+++ dataflow_cookiecutter/cli/ls.py
@@ -10,10 +10,7 @@ from tabulate import tabulate
 from loguru import logger
 
 from .mixins import configure_logger
-
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-INDEX_FILE = os.path.join(ROOT, "index.json")
+from .config import TEMPLATE_CONFIG
 
 
 @click.command()
@@ -25,10 +22,7 @@ def ls(ctx):
         debug_file=ctx.obj["DEBUG_FILE"],
     )
 
-    logger.debug(f"Loading index file: {INDEX_FILE}")
-    with open(INDEX_FILE) as f:
-        data = json.load(f)
-
-    data_ = [[k] + v for k, v in data.items()]
+    logger.debug(f"Loading configuration")
+    data = [[k] + v for k, v in TEMPLATE_CONFIG.items()]
     headers = ["Name", "Dir", "Description"]
-    print(tabulate(data_, headers=headers))
+    print(tabulate(data, headers=headers))
